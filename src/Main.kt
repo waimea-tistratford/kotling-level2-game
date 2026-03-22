916b4bfc60d38863ab31551959ad09dbd810b684
waimea-tistratford <tistratford@waimea.school.nz>
2026-03-23 11:00:40 +1300

    Update Main.kt
    Added functions

--- src/Main.kt
+++ src/Main.kt
@@ -2,20 +2,57 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
+ * Project Name:   Pinned
+ * Project Author: Troy Stratford
  * GitHub Repo:    GITHUB REPO URL HERE
  * ---------------------------------------------------------------------
  * Notes:
  * PROJECT NOTES HERE
  * =====================================================================
  */
-
+val spaces = mutableListOf<String>()
 
 
 fun main() {
+    createBoard()
+    showBoard()
+
+}
+
+// Creating the board -----------------------------------------------------
+
+fun createBoard() {
+    while (spaces.size < 16)
+    spaces.add("")
+}
+
+fun addCounters() {
+    var counter = "x"
+    repeat(4) {
+
+    }
 
 
 }
 
-fun
+
+
+fun showBoard() {
+    println()
+
+    println()
+    print("┌───")
+    print("┬───".repeat(spaces.size - 1))
+    println("┐")
+
+
+    for (i in 0..<spaces.size) {
+        print("│")
+        print(" ${spaces[i]}".padEnd(3))
+    }
+    println("│")
+
+    print("└───")
+    print("┴───".repeat(spaces.size - 1))
+    println("┘")
+}
