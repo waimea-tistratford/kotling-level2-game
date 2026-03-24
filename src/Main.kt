2e05942b650de895c13574bf3fb9c7e486bf3bf9
waimea-tistratford <tistratford@waimea.school.nz>
2026-03-24 15:10:24 +1300

    Update Main.kt
    Completed board setup which included placing   4 x counters in random places and making sure they were viable
    and the 1 o counter and names

--- src/Main.kt
+++ src/Main.kt
@@ -10,49 +10,101 @@
  * PROJECT NOTES HERE
  * =====================================================================
  */
-val spaces = mutableListOf<String>()
-
+var board = mutableListOf<String>()
+var player1Name: String = ""
+var player2Name: String = ""
 
 fun main() {
+    playerNames()
     createBoard()
+    addCounters()
     showBoard()
+
+}
+
+fun playerNames() {
+    print("Player 1 Name: ")
+    player1Name = readln()
+
+
+
+    print("Player 2 Name: ")
+    player2Name = readln()
+
 
 }
 
 // Creating the board -----------------------------------------------------
 
 fun createBoard() {
-    while (spaces.size < 16)
-    spaces.add("")
+    while (board.size < 16) {
+        board.add(" ")
+    }
 }
 
 fun addCounters() {
-    var counter = "x"
-    repeat(4) {
+    val x_counter = "x"
 
+    repeat(4) {
+        while (true) {
+            val x_counterPosition = (1..<board.size).random()
+            if (board[x_counterPosition] == " ") {
+                board[x_counterPosition] = x_counter
+                break
+            }
+        }
     }
 
+    val o_counter = "o"
 
+    repeat(1) {
+        while (true) {
+            val o_counterPosition = (1..<board.size).random()
+            if (board[o_counterPosition] == " ") {
+                board[o_counterPosition] = o_counter
+                break
+            }
+        }
+    }
 }
+
 
 
 
 fun showBoard() {
     println()
 
+    println("$player1Name vs $player2Name")
+
+    println()
+
+    print("  1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16")
+
     println()
     print("┌───")
-    print("┬───".repeat(spaces.size - 1))
+    print("┬───".repeat(board.size - 1))
     println("┐")
 
 
-    for (i in 0..<spaces.size) {
+    for (i in 0..<board.size) {
         print("│")
-        print(" ${spaces[i]}".padEnd(3))
+        print(" ${board[i]}".padEnd(3))
     }
     println("│")
 
     print("└───")
-    print("┴───".repeat(spaces.size - 1))
+    print("┴───".repeat(board.size - 1))
     println("┘")
 }
+
+fun player1Action() {
+
+}
+
+fun player2Action() {
+
+}
+
+
+
+
